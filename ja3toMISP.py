453469abb25870ceafcacdb32bbec05dd95e6bb5
eCrimeLabs <rand@ecrime.dk>
2018-12-29 11:31:11 +0100

    updated

--- ja3toMISP.py
+++ ja3toMISP.py
@@ -32,11 +32,14 @@ import dpkt
 import json
 import socket
 import struct
+import sys
+import pprint
+from datetime import datetime
 from hashlib import md5
 from pymisp import MISPObject
 from pymisp import PyMISP
 from pymisp import MISPEvent
-from keys import misp_url, misp_key, misp_verifycert, vt_url, vt_key
+from keys import misp_url, misp_key, misp_verifycert
 
 __author__ = "Dennis Rand - eCrimeLabs"
 __copyright__ = "Copyright (c) 2019, eCrimeLabs"
@@ -54,10 +57,58 @@ SSL_PORT = 443
 TLS_HANDSHAKE = 22
 
 def splash():
-    print ('JA3 fingerprint to MISP')
-    print ('(c)2019 eCrimeLabs')
+    print ("\r\n")
+    print ('JA3 fingerprint to MISP Objects')
+    print ('(c)2018 eCrimeLabs')
     print ('https://www.ecrimelabs.com')
     print ("----------------------------------------\r\n")
+
+def init(misp_url, misp_key):
+    return PyMISP(misp_url, misp_key, misp_verifycert, 'json', debug=False)
+
+def create_misp_objects(ja3_objects, misp_event, pcap_filename, misp, to_ids):
+    #create_misp_objects(ja3_object, misp_event, pcap_filename)
+    event = MISPEvent()
+    event.from_dict(**misp_event)
+
+    # is_in_misp_event(misp_event, ja3_checksum)
+    print ("- Creating object(s)")
+#    pprint.pprint(ja3_object)
+    for ja3_object in ja3_objects:
+        ja3_digest = (ja3_objects[ja3_object]['ja3_digest'])
+        destination_ip = (ja3_objects[ja3_object]['destination_ip'])
+        source_ip = (ja3_objects[ja3_object]['source_ip'])
+        timestamp = (ja3_objects[ja3_object]['time'])
+
+        if not ( is_in_misp_event(misp_event, ja3_digest, destination_ip) ):
+            print ("\t " + ja3_digest + " -> " + destination_ip + " -> " + source_ip)
+            misp_object = event.add_object(name='ja3', comment=pcap_filename, distribution=5, standalone=False)
+            obj_attr = misp_object.add_attribute('ja3-fingerprint-md5', value=ja3_digest, distribution=5)
+            misp_object.add_attribute('ip-src', value=source_ip, to_ids=to_ids, distribution=5)
+            misp_object.add_attribute('ip-dst', value=destination_ip, to_ids=to_ids, distribution=5)
+            misp_object.add_attribute('first-seen', value=timestamp, disable_correlation=True, distribution=5)
+
+    try:
+        misp.update(event)
+    except (KeyError, RuntimeError, TypeError, NameError):
+        print ("An error occoured when updating the event")
+        sys.exit()
+
+    print ("- The MISP objects seems to have been added correctly to the event.... \r\n\r\n")
+
+def is_in_misp_event(misp_event, ja3_digest, destination_ip):
+    found = False
+    for obj_loop in misp_event['Object']:
+        found_ja3 = False
+        found_ip = False
+        for attr_loop in obj_loop['Attribute']:
+            if(attr_loop['value'] == ja3_digest):
+                found_ja3 = True
+            elif(attr_loop['value'] == destination_ip):
+                found_ip = True
+        if( (found_ja3) and found_ip ):
+            found = True
+    return(found)
 
 def convert_ip(value):
     """Convert an IP address from binary to text.
@@ -169,7 +220,8 @@ def process_pcap(pcap, any_port=False):
     :param any_port: Whether or not to search for non-SSL ports
     :type any_port: bool
     """
-    results = list()
+
+    results = dict() # list()
     for timestamp, buf in pcap:
         try:
             eth = dpkt.ethernet.Ethernet(buf)
@@ -235,53 +287,78 @@ def process_pcap(pcap, any_port=False):
             ja3.append(convert_to_ja3_segment(buf, 2))
             ja3 += process_extensions(client_handshake)
             ja3 = ",".join(ja3)
+            ja3_digest = md5(ja3.encode()).hexdigest()
 
-            record = {"source_ip": convert_ip(ip.src),
+            data = dict()
+
+            data = {"source_ip": convert_ip(ip.src),
                       "destination_ip": convert_ip(ip.dst),
                       "source_port": tcp.sport,
                       "destination_port": tcp.dport,
                       "ja3": ja3,
-                      "ja3_digest": md5(ja3.encode()).hexdigest(),
-                      "timestamp": timestamp}
-            results.append(record)
+                      "ja3_digest": ja3_digest,
+                      "timestamp": timestamp,
+                      "time": datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%dT%H:%M:%SZ')}
+
+            results[ja3_digest + '-' + convert_ip(ip.dst)] = data
 
     return results
 
 
 def main():
+    splash()
     """Intake arguments from the user and print out JA3 output."""
-    desc = "A python script for extracting JA3 fingerprints from PCAP files"
+    desc = "Extracting JA3 fingerprints from PCAP files, and importing into MISP as objects"
     parser = argparse.ArgumentParser(description=(desc))
-    parser.add_argument("pcap", help="The pcap file to process")
-    help_text = "Look for client hellos on any port instead of just 443"
+    parser.add_argument("-f", "--file", required=True, help="The pcap file to process")
     parser.add_argument("-a", "--any_port", required=False,
                         action="store_true", default=False,
-                        help=help_text)
-    help_text = "Print out as JSON records for downstream parsing"
+                        help="Look for client hellos on any port instead of just 443")
     parser.add_argument("-j", "--json", required=False, action="store_true",
-                        default=True, help=help_text)
+                        help="Print out as JSON records for downstream parsing or for debug reasons")
+    parser.add_argument("-c", "--create", required=False, type=str,
+                        help="Create a new MISP event with the specified name")
+    parser.add_argument("-u", "--uuid", required=False, type=str,
+                        help="Add to an allready existing event (input has to be UUID)")
+    parser.add_argument("-i", "--ids", required=False, action="store_true", default=False,
+                        help="Adds the to_ids to the source and destination IP's")
     args = parser.parse_args()
 
     # Use an iterator to process each line of the file
-    output = None
-    with open(args.pcap, 'rb') as fp:
+    ja3_objects = None
+    pcap_filename = args.file
+    to_ids = args.ids
+    with open(args.file, 'rb') as fp:
         try:
             capture = dpkt.pcap.Reader(fp)
         except ValueError as e:
             raise Exception("File doesn't appear to be a PCAP: %s" % e)
-        output = process_pcap(capture, any_port=args.any_port)
+        ja3_objects = process_pcap(capture, any_port=args.any_port)
 
-    if args.json:
-        output = json.dumps(output, indent=4, sort_keys=True)
-        print(output)
+    if (args.json):
+        ja3_objects = json.dumps(ja3_objects, indent=4, sort_keys=True)
+        print(ja3_objects)
+    elif (args.uuid):
+        # Add to existing UUID
+        try:
+            misp = init(misp_url, misp_key)
+            misp_event = misp.get_event(args.uuid)['Event']
+        except KeyError as e:
+            print ("An error occoured getting the UUID, either connection issues or UUID does not exits.")
+            sys.exit(0)
+        create_misp_objects(ja3_objects, misp_event, pcap_filename, misp, to_ids)
+    elif (args.create):
+        # Create a new event in MISP
+        pass
     else:
-        for record in output:
-            tmp = '[{dest}:{port}] JA3: {segment} --> {digest}'
-            tmp = tmp.format(dest=record['destination_ip'],
-                             port=record['destination_port'],
-                             segment=record['ja3'],
-                             digest=record['ja3_digest'])
-            print(tmp)
+        pass
+#        for record in ja3_objects:
+#            tmp = '[{dest}:{port}] JA3: {segment} --> {digest}'
+#            tmp = tmp.format(dest=record['destination_ip'],
+#                             port=record['destination_port'],
+#                             segment=record['ja3'],
+#                             digest=record['ja3_digest'])
+#            print(tmp)
 
 
 if __name__ == "__main__":
